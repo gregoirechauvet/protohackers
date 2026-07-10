6b4887b7db14d12c7367003bba0c2ac1cc4ce3a7
Grégoire Chauvet <6739268+gregoirechauvet@users.noreply.github.com>
2026-07-11 00:12:49 +0200

    Iterate on solution with io_uring

--- budget-chat/src/main.rs
+++ budget-chat/src/main.rs
@@ -1,62 +1,147 @@
-use std::io::{BufReader, BufWriter, Error, Read, Write};
-use std::net::{Shutdown, SocketAddr, TcpListener, TcpStream};
-use std::thread;
+use io_uring::cqueue::Entry;
+use io_uring::{IoUring, opcode, types, SubmissionQueue};
+use std::collections::HashMap;
+use std::io::Error;
+use std::net::TcpListener;
+use std::os::fd::AsRawFd;
 
-fn main() {
-    let listener = TcpListener::bind("0.0.0.0:8080").unwrap();
+const SUBMISSION_QUEUE_SIZE: u32 = 16; // Need to be a power of two
+
+fn main() -> Result<(), Error> {
+    let mut ring = IoUring::new(SUBMISSION_QUEUE_SIZE)?;
+    let (submitter, mut submission_queue, mut completion_queue) = ring.split();
+
+    let listener = TcpListener::bind("0.0.0.0:8080")?;
+    let listener_fd = listener.as_raw_fd();
+
+    let accept_submission = opcode::AcceptMulti::new(types::Fd(listener_fd))
+        .build()
+        .user_data(listener_fd as _);
+
+    unsafe {
+        if let Err(_) = submission_queue.push(&accept_submission) {
+            return Err(Error::other("io_uring submission queue is full"));
+        }
+    }
+
+    submission_queue.sync();
 
     println!("Server listening...");
 
-    for stream in listener.incoming() {
-        let stream = match stream {
-            Ok(s) => s,
-            Err(e) => {
-                eprintln!("Failed to accept connection: {e}");
-                continue;
-            }
-        };
+    let mut clients: HashMap<i32, Client> = HashMap::new();
 
-        thread::spawn(|| {
-            let _ = handle_connection(stream);
-        });
-    }
-}
+    loop {
+        submitter.submit_and_wait(1)?; // Might need to continue on libc::EINTR errors according to Gemini
+        completion_queue.sync();
 
-struct ConnectionHandler {
-    stream: TcpStream,
-    peer_addr: SocketAddr,
-}
+        for entry in &mut completion_queue {
+            match entry.user_data() {
+                x if x == listener_fd as u64 => {
+                    let client_fd = match handle_accept_event(entry) {
+                        Ok(client_fd) => client_fd,
+                        Err(_) => {
+                            eprintln!("Error accepting connection");
+                            continue;
+                        }
+                    };
 
-impl ConnectionHandler {
-    fn new(stream: TcpStream) -> Result<Self, Error> {
-        let peer_addr = stream.peer_addr()?;
-        println!("[Client {peer_addr}] Connected");
-        Ok(ConnectionHandler { stream, peer_addr })
-    }
+                    match setup_client(&mut submission_queue, client_fd) {
+                        Ok(client) => {
+                            clients.insert(client_fd, client);
+                        }
+                        Err(_) => {
+                            eprintln!("Cannot read client socket");
+                            continue;
+                        }
+                    };
+                }
+                x => {
+                    let client_fd = x as i32;
+                    let result = entry.result();
 
-    fn handle(&mut self) -> Result<(), Error> {
-        let mut reader = BufReader::new(&self.stream);
-        let mut writer = BufWriter::new(&self.stream);
+                    if let Some(client) = clients.remove(&client_fd) {
+                        if result <= 0 {
+                            // TODO: Shutdown client socket
+                            println!("Client disconnected or error");
+                            continue;
+                        }
 
-        loop {
-            // if let Err(_) = reader.read_line(&mut buffer) {
-            //     break;
-            // }
+                        let bytes_read = result as usize;
+                        if let Ok(value) = str::from_utf8(&client.buffer[..bytes_read]) {
+                            println!("Got message: {value}");
+                        }
+
+                        clients.insert(client_fd, client);
+                    } else {
+                        eprintln!("Got message from unknown client: {client_fd}");
+                    }
+                }
+            };
         }
-
-        Ok(())
     }
 }
 
-impl Drop for ConnectionHandler {
-    fn drop(&mut self) {
-        println!("[Client {}] Shutting down", self.peer_addr);
-        if let Err(e) = self.stream.shutdown(Shutdown::Both) {
-            eprintln!("[Client {}] Error shutting down: {}", self.peer_addr, e);
+fn handle_accept_event(entry: Entry) -> Result<i32, Error> {
+    let client_fd = entry.result();
+    if client_fd < 0 {
+        return Err(Error::from_raw_os_error(client_fd));
+    }
+
+    println!("Accepting client with fd: {client_fd}");
+
+    Ok(client_fd)
+}
+
+fn setup_client(
+    submission_queue: &mut SubmissionQueue,
+    client_fd: i32,
+) -> Result<Client, Error> {
+    let mut client = Client::new(client_fd);
+
+    let welcome_message = b"Welcome to chat! What shall I call you?\n";
+    let welcome_submission = opcode::Write::new(types::Fd(client_fd), welcome_message.as_ptr(), welcome_message.len() as _).
+        build().
+        user_data(client_fd as _);
+
+    let read_submission =
+        opcode::Read::new(types::Fd(client_fd), client.buffer.as_mut_ptr(), client.buffer.len() as _)
+            .build()
+            .user_data(client_fd as _);
+
+    unsafe {
+        if let Err(_) = submission_queue.push_multiple(&[welcome_submission, read_submission]) {
+            return Err(Error::other("io_uring submission queue is full"));
         }
     }
+
+    submission_queue.sync();
+
+    Ok(client)
 }
 
-fn handle_connection(stream: TcpStream) -> Result<(), Error> {
-    ConnectionHandler::new(stream)?.handle()
+enum State {
+    Pending,
+    Joined { name: String },
+}
+
+struct Client {
+    client_fd: i32,
+    buffer: Vec<u8>, // Use Vec to allocate memory on the heap and keep stable memory pointer
+
+    state: State,
+}
+
+impl Client {
+    fn new(client_fd: i32) -> Self {
+        Client {
+            client_fd,
+            buffer: vec![0u8; 4096],
+
+            state: State::Pending
+        }
+    }
+
+    fn join(&mut self, name: String) {
+        self.state = State::Joined { name }
+    }
 }
