2cd09197dd0a63b26713651bceceb453912c5582
Grégoire Chauvet <6739268+gregoirechauvet@users.noreply.github.com>
2026-07-11 15:04:00 +0200

    Collect submission entries to submit at once

--- budget-chat/src/main.rs
+++ budget-chat/src/main.rs
@@ -1,5 +1,6 @@
-use io_uring::cqueue::Entry;
-use io_uring::{opcode, types, IoUring};
+use io_uring::cqueue::Entry as CompletionEntry;
+use io_uring::squeue::Entry as SubmissionEntry;
+use io_uring::{IoUring, opcode, types};
 use std::collections::HashMap;
 use std::io::Error;
 use std::net::TcpListener;
@@ -84,45 +85,39 @@ impl ChatRoom {
     }
 
     fn start(&mut self) -> Result<(), Error> {
-        let mut entries = Vec::with_capacity(256);
+        let mut completion_entries = Vec::<CompletionEntry>::with_capacity(256);
+        let mut submission_entries = Vec::<SubmissionEntry>::with_capacity(256);
 
         loop {
             self.ring.submitter().submit_and_wait(1)?; // Might need to continue on libc::EINTR errors according to Gemini
 
-            entries.clear();
+            completion_entries.clear();
+            submission_entries.clear();
+
             for entry in self.ring.completion() {
-                entries.push(entry);
+                completion_entries.push(entry);
             }
 
-            for entry in entries.drain(..) {
-                match user_data_op(entry.user_data()) {
-                    Some(Op::Accept) => {
-                        println!("Accepting client...");
-                        self.handle_accept(entry)?;
-                    }
-                    Some(Op::Read) => {
-                        println!("Read client...");
-                        self.handle_read(entry)?;
-                    }
-                    Some(Op::Write) => {
-                        println!("Write client...");
-                    }
-                    Some(Op::Close) => {
-                        println!("Successfully closed client");
-                    },
-                    None => {
-                        eprintln!("Unknown operation");
-                    }
+            for entry in completion_entries.drain(..) {
+                let entries = match user_data_op(entry.user_data()) {
+                    Some(Op::Accept) => self.handle_accept(entry),
+                    Some(Op::Read) => self.handle_read(entry),
+                    Some(Op::Write) => self.handle_write(entry),
+                    Some(Op::Close) => self.handle_close(entry),
+                    None => Err(Error::other("Unknown operation")),
+                };
+                submission_entries.extend(entries?);
+            }
+
+            unsafe {
+                if let Err(_) = self.ring.submission().push_multiple(&submission_entries) {
+                    return Err(Error::other("io_uring submission queue is full"));
                 }
             }
         }
     }
 
-    fn broadcast(&self, message: String) {
-
-    }
-
-    fn handle_accept(&mut self, entry: Entry) -> Result<(), Error> {
+    fn handle_accept(&mut self, entry: CompletionEntry) -> Result<Vec<SubmissionEntry>, Error> {
         let client_fd = entry.result();
         if client_fd < 0 {
             return Err(Error::from_raw_os_error(-client_fd));
@@ -139,27 +134,21 @@ impl ChatRoom {
             WELCOME_MESSAGE.as_ptr(),
             WELCOME_MESSAGE.len() as _,
         )
-            .build()
-            .user_data(make_user_data(Op::Write, client_fd));
+        .build()
+        .user_data(make_user_data(Op::Write, client_fd));
 
         let read_submission = opcode::Read::new(
             types::Fd(client_fd),
             client.buffer.as_mut_ptr(),
             client.buffer.len() as _,
         )
-            .build()
-            .user_data(make_user_data(Op::Read, client_fd));
+        .build()
+        .user_data(make_user_data(Op::Read, client_fd));
 
-        unsafe {
-            if let Err(_) = self.ring.submission().push_multiple(&[welcome_submission, read_submission]) {
-                return Err(Error::other("io_uring submission queue is full"));
-            }
-        }
-
-        Ok(())
+        Ok(vec![welcome_submission, read_submission])
     }
 
-    fn handle_read(&mut self, entry: Entry) -> Result<(), Error> {
+    fn handle_read(&mut self, entry: CompletionEntry) -> Result<Vec<SubmissionEntry>, Error> {
         let client_fd = user_data_id(entry.user_data());
         let result = entry.result();
 
@@ -173,20 +162,19 @@ impl ChatRoom {
         if result <= 0 {
             println!("Disconnecting client {}...", client.client_fd);
             client.disconnect();
-            self.close_client(client)?;
-            return Ok(());
+            return Ok(vec![self.close_client(client_fd)]);
         }
 
         let bytes_read = result as usize;
         let message = match str::from_utf8(&client.buffer[..bytes_read]) {
             Err(_) => {
                 println!("Got invalid UTF-8 message from client {}", client.client_fd);
-                return Ok(());
+                return Err(Error::other("Got invalid UTF-8 message from client"));
             }
             Ok(value) => {
                 println!("Got message: {value}");
                 value
-            },
+            }
         };
 
         let stuff = match &client.state {
@@ -202,25 +190,59 @@ impl ChatRoom {
             }
         };
 
-        // client.join(message);
-
-        self.broadcast(stuff);
-
-        Ok(())
+        Ok(self.broadcast(stuff, client_fd))
     }
 
-    fn close_client(&mut self, client: &Client) -> Result<(), Error> {
-        let close_submission = opcode::Close::new(types::Fd(client.client_fd))
-            .build()
-            .user_data(make_user_data(Op::Close, client.client_fd));
+    fn handle_write(&mut self, entry: CompletionEntry) -> Result<Vec<SubmissionEntry>, Error> {
+        let client_fd = user_data_id(entry.user_data());
+        let result = entry.result();
+        if result < 0 {
+            eprintln!("Error writing to client {client_fd}");
+            // Probably disconnect client
+            return Err(Error::from_raw_os_error(-result));
+        }
+        Ok(vec![])
+    }
 
-        unsafe {
-            if let Err(_) = self.ring.submission().push(&close_submission) {
-                return Err(Error::other("io_uring submission queue is full"));
-            }
+    fn handle_close(&mut self, entry: CompletionEntry) -> Result<Vec<SubmissionEntry>, Error> {
+        let client_fd = user_data_id(entry.user_data());
+        let result = entry.result();
+        if result < 0 {
+            eprintln!("Error closing client {client_fd}");
+            return Err(Error::from_raw_os_error(-result));
         }
 
-        Ok(())
+        println!("Client {client_fd} disconnected");
+        self.clients.remove(&client_fd);
+
+        Ok(vec![])
+    }
+
+    fn broadcast(&self, message: String, sender: i32) -> Vec<SubmissionEntry> {
+        self.clients
+            .iter()
+            .filter(|(client_fd, client)| {
+                if **client_fd == sender {
+                    return false;
+                }
+
+                if let State::Joined { name: _ } = &client.state {
+                    return true;
+                }
+                false
+            })
+            .map(|(client_fd, _)| {
+                opcode::Write::new(types::Fd(*client_fd), message.as_ptr(), message.len() as _)
+                    .build()
+                    .user_data(make_user_data(Op::Write, *client_fd))
+            })
+            .collect::<Vec<_>>()
+    }
+
+    fn close_client(&self, client_fd: i32) -> SubmissionEntry {
+        opcode::Close::new(types::Fd(client_fd))
+            .build()
+            .user_data(make_user_data(Op::Close, client_fd))
     }
 }
 
@@ -248,7 +270,9 @@ impl Client {
     }
 
     fn join(&mut self, name: &str) {
-        self.state = State::Joined { name: String::from(name) }
+        self.state = State::Joined {
+            name: String::from(name),
+        }
     }
 
     fn disconnect(&mut self) {
